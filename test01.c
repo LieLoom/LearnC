d416fbb07fb197d8470b298ca3f1b55647845786
LieLoom <51847604+LieLoom@users.noreply.github.com>
2019-06-15 16:13:34 +0900

    Update test01.c

--- test01.c
+++ test01.c
@@ -1,1 +1,13 @@
 /*this is my first C*/
+#include <stdio.h>
+int main(void){
+  int x,y,z;
+  printf("整数入力して");
+  scanf("%d", x);
+  printf("もいっこ入力して");
+  scanf("%d", y);
+  z=x+y;
+  printf("和は%dだね！", z);
+  return 0;
+}
+  
